f5a40b8d6621fc52c3926442a35556bc60c3f427
MirekKopec <mkopec95@gmail.com>
2017-12-03 13:56:48 +0100

    implementing eventEmitter

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 var OSinfo = require('./modules/OSinfo');
+var EventEmitter = require('events').EventEmitter;
+
+var emitter = new EventEmitter();
+
+emitter.on('beforeCommand', function(instruction) {
+    console.log('You wrote: ' + instruction + ' trying to run command.')
+});
+
+emitter.on('afterCommand', function() {
+console.log('Finished command');
+});
+
 
 process.stdin.setEncoding('utf-8');
 
@@ -10,8 +22,11 @@ process.stdin.on('readable', function() {
     var input = process.stdin.read();
 
     if (input !== null) {
-
+        
+        // odpalanie zdarzenia beforeCommand (z parametrem)
         var instruction = input.toString().trim();
+
+         emitter.emit('beforeCommand', instruction);
         
         switch (instruction) {
         
@@ -34,7 +49,10 @@ process.stdin.on('readable', function() {
 
             default: 
             process.stderr.write('Wrong instruction!\n');
-        }
+        };
+
+         // emitowanie zdarzenia afterCommand (bez parametru)
+        emitter.emit('afterCommand');
 
     }
 });
